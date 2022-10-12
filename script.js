54f13df8c936e930c93fc485f083ca7b26fb5de3
Pascal P <67502663+PascalP1998@users.noreply.github.com>
2022-10-12 13:54:40 +0200

    Finished Hamburger Menu
    Also trying to add my image on top of the main div

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function hamburgerMenu() {
     let element = document.getElementById("hamburger-menu");
-    if (element.style.visibility === "hidden") {
-        element.style.visibility = "visible";
-        element.style.zIndex = "10";
+    if (element.style.visibility === "visible") {
+        element.style.visibility = "hidden";
+        element.style.zIndex = "-1000000";
         // element.style.position = "relative";
     } else {
-        element.style.visibility = "hidden";
-        element.style.zIndex = "-10000";
-        element.style.position = "fixed";
+        element.style.visibility = "visible";
+        element.style.zIndex = "10";
+        // element.style.position = "fixed";
     }
 }
