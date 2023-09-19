b3b2883829d74314d9978fa9f738bebca95d4c81
Pascal P <67502663+PascalP1998@users.noreply.github.com>
2023-09-19 18:05:11 +0200

    Added blog functionality

---
+++ blog.js
@@ -0,0 +1,36 @@
+function getBlogposts() {
+    return new Promise((resolve, reject) => {
+        fetch("blogposts.json")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Fehler beim Laden der JSON-Daten");
+                }
+                return response.json();
+            })
+            .then(blogposts => {
+                resolve(blogposts); // Promise auflösen und Daten übergeben
+            })
+            .catch(error => {
+                reject(error); // Fehlerbehandlung, falls etwas schief geht
+            });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getBlogposts().then(blogposts => {
+        const savedBlogposts = blogposts.posts;
+        const all_view = document.getElementById("all-posts");
+        for (let postKey in savedBlogposts) {
+            const post = savedBlogposts[postKey];
+            const postElement = document.createElement("div");
+            postElement.classList.add("blogpost");
+            postElement.innerHTML = `
+                <h2>${post.title}</h2>
+                <h3>${post.date}</h3>
+                <img src="${post.image}" alt="${post.title}" />
+                <p class="font-serif">${post.text}</p>
+                `;
+            all_view.appendChild(postElement);
+        }
+    });
+});
