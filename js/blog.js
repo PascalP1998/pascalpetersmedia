d1ab0765d092406c2fe396effca339bd4be59fde
Pascal P <67502663+PascalP1998@users.noreply.github.com>
2023-09-21 14:57:21 +0200

    Changed blogpost link AGAIN

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 function getBlogposts() {
     return new Promise((resolve, reject) => {
-        fetch('blogposts.json')
+        fetch('https://github.com/PascalP1998/pascalpetersmedia/js/blogposts.json')
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Fehler beim Laden der JSON-Daten");
